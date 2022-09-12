b4fc6d4e6c32544de3e531fc40ca1f7c5287298e
Lucas-Masaba <masabaluke@gmail.com>
2022-09-12 23:32:29 +0300

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_171919) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_12_202803) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_171919) do
     t.string "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role", default: "User"
   end
 
 end
