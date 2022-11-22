a0b800d79dda266fbb72b1db99ae20f0936da4cd
Lucas-Masaba <masabaluke@gmail.com>
2022-11-22 19:48:06 +0300

    setup devise with rails g devise_token_auth:install User auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+        # extend Devise::Models
+            # Include default devise modules.
+            devise :database_authenticatable, :registerable,
+                    :recoverable, :rememberable, :trackable, :validatable,
+                    :confirmable, :omniauthable
+            include DeviseTokenAuth::Concerns::User
   require "securerandom"
 
   has_secure_password
